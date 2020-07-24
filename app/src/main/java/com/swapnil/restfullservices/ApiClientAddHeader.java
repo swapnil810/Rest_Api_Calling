5495129c1fb48fecb5454ded6512a9891a0ae402
swapnil810 <sharmaswapnil810@gmail.com>
2020-07-24 17:00:26 +0530

    Adding header all api's when calling ApiClient

---
+++ app/src/main/java/com/swapnil/restfullservices/ApiClientAddHeader.java
@@ -0,0 +1,53 @@
+package com.swapnil.restfullservices;
+
+import com.facebook.stetho.okhttp3.StethoInterceptor;
+import com.swapnil.utills.MyConstants;
+
+import java.io.IOException;
+import java.util.TimeZone;
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.Interceptor;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+/**
+ * This class is used as .
+ *
+ * @author CanopusInfoSystems
+ * @version 1.0
+ * @since 24/7/20 :July : 2020 on 16 : 58.
+ */
+class ApiClientAddHeader {
+    public static final String base_url = "#";  //client's url
+
+    static final OkHttpClient okhttpClient = new OkHttpClient.Builder().addInterceptor(new Interceptor() {
+        @Override
+        public Response intercept(Interceptor.Chain chain) throws IOException {
+            Request newRequest = chain.request().newBuilder()
+                    .addHeader(MyConstants.HEADER_TIME_ZONE, TimeZone.getDefault().getID())
+                    .build();
+            return chain.proceed(newRequest);
+        }
+    }).readTimeout(5000, TimeUnit.SECONDS)
+            .writeTimeout(5000, TimeUnit.SECONDS)
+            .connectTimeout(5000, TimeUnit.SECONDS)
+            .addNetworkInterceptor(new StethoInterceptor()).build();
+
+    private static Retrofit retrofit = null;
+
+    public static Retrofit getClient() {
+        if (retrofit == null) {
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(base_url).client(okhttpClient)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+        return retrofit;
+    }
+
+}
+
--- app/src/main/java/com/swapnil/utills/MyConstants.java
+++ app/src/main/java/com/swapnil/utills/MyConstants.java
@@ -76,6 +76,7 @@ public class MyConstants {
     public static final String HIPS = "neck";
     public static final String MY_FILE_PATH = "my_file_path";
     public static final String PROFILE_PIC = "profile_pic";
+    public static final String HEADER_TIME_ZONE = "Time-Zone";
     public static Dialog progressDialog;
 
     /**
